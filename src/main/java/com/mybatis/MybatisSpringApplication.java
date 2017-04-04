2c86eb698dda840474a8079b588d98ef2474b101
江彦 <mrjiangyan@hotmail.com>
2017-04-04 20:53:18 +0800

    修改事务的注解

--- src/main/java/com/mybatis/MybatisSpringApplication.java
+++ src/main/java/com/mybatis/MybatisSpringApplication.java
@@ -3,11 +3,11 @@ package com.mybatis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ImportResource("beans.xml")
-@Transactional
+@EnableTransactionManagement
 public class MybatisSpringApplication {
 
     public static void main(String[] args) {
